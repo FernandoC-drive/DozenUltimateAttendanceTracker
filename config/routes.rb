4423385cf699e66ba98fbd241b651810fe89e1f9
FernandoC-drive <fernando.c@tamu.edu>
2026-02-15 14:58:03 -0600

    fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  # --- 1. The Home Page ---
-  root 'members#index'
-
-  # --- 2. Resources ---
-  # These allow you to Create, Read, Update, and Delete data
-  resources :members
-  resources :attendance_records
-  resources :weekly_workouts
-
-  # --- Health Check (Standard in Rails 8) ---
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   resource :session, only: %i[new create destroy]
 
